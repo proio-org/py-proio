3d6216723f3a80bfa79de37275d9f45627564f66
David Blyth <dblyth@anl.gov>
2018-09-26 13:02:34 -0500

    proio/reader.py: skip now operates in a cleaner way like in go-proio and cpp-proio

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -3,7 +3,7 @@ import pytest
 
 import proio
 
-def test_next_header1():
+def test_header1():
     buf = io.BytesIO(b'')
     with proio.Writer(fileobj = buf) as writer:
         event = proio.Event()
@@ -15,32 +15,9 @@ def test_next_header1():
     buf.seek(0, 0)
 
     with proio.Reader(fileobj = buf) as reader:
-        header = reader.next_header()
-        assert(header.nEvents == 2)
-        assert(reader.__next__() != None)
-        try:
-            reader.__next__()
-            assert(False)
-        except StopIteration:
-            pass
-
-def test_next_header2():
-    buf = io.BytesIO(b'')
-    with proio.Writer(fileobj = buf) as writer:
-        event = proio.Event()
-        writer.push(event)
-        writer.push(event)
-        writer.flush()
-        writer.push(event)
-
-    buf.seek(0, 0)
-
-    with proio.Reader(fileobj = buf) as reader:
-        assert(reader.__next__() != None)
-        header = reader.next_header()
-        assert(header.nEvents == 1)
-        try:
-            reader.__next__()
-            assert(False)
-        except StopIteration:
-            pass
+        reader.skip(0)
+        assert(reader._bucket_header.nEvents == 2)
+        reader.skip(2)
+        assert(reader._bucket_header.nEvents == 1)
+        reader.skip(1)
+        assert(reader._bucket_header is None)
