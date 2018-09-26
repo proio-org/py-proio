c63eddf5f2dbc120cceda089e685179710bd7232
David Blyth <dblyth@anl.gov>
2018-09-26 15:15:24 -0500

    Added metadata handling capability
    Metadata are associated with Events as they are read.
    
    Also, metadata assigned to Events are pushed to the stream automatically.

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -21,3 +21,47 @@ def test_header1():
         assert(reader._bucket_header.nEvents == 1)
         reader.skip(1)
         assert(reader._bucket_header is None)
+
+def test_push_update1():
+    buf = io.BytesIO(b'')
+    with proio.Writer(fileobj = buf) as writer:
+        writer.push_metadata('key1', b'value1')
+        writer.push_metadata('key2', b'value2')
+        event = proio.Event()
+        writer.push(event)
+        writer.push_metadata('key2', b'value3')
+        writer.push(event)
+        writer.push_metadata('key1', b'value4')
+        writer.push_metadata('key2', b'value5')
+        writer.push(event)
+
+    buf.seek(0, 0)
+    with proio.Reader(fileobj = buf) as reader:
+        event1 = next(reader)
+        event2 = next(reader)
+        event3 = next(reader)
+        assert(event1.metadata['key1'] == b'value1')
+        assert(event1.metadata['key2'] == b'value2')
+        assert(event2.metadata['key1'] == b'value1')
+        assert(event2.metadata['key2'] == b'value3')
+        assert(event3.metadata['key1'] == b'value4')
+        assert(event3.metadata['key2'] == b'value5')
+
+    buf = io.BytesIO(b'')
+    with proio.Writer(fileobj = buf) as writer:
+        writer.push(event1)
+        writer.push(event2)
+        writer.push(event3)
+
+    buf.seek(0, 0)
+    with proio.Reader(fileobj = buf) as reader:
+        event1 = next(reader)
+        event2 = next(reader)
+        event3 = next(reader)
+        assert(event1.metadata['key1'] == b'value1')
+        assert(event1.metadata['key2'] == b'value2')
+        assert(event2.metadata['key1'] == b'value1')
+        assert(event2.metadata['key2'] == b'value3')
+        assert(event3.metadata['key1'] == b'value4')
+        assert(event3.metadata['key2'] == b'value5')
+
